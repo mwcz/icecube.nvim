592caa601de3f517a6c5c8e74f541bcbf72ea6e2
Michael Clayton <mwc@clayto.com>
2024-05-29 09:53:08 -0400

    set rust macros and decorator colors

--- lua/icecube/highlights/lsp.lua
+++ lua/icecube/highlights/lsp.lua
@@ -4,6 +4,8 @@ return {
 	-- ["@lsp.mod.constant.rust"] = c.Constant,
 	-- ["@lsp.type.builtinType.rust"] = { fg = colors.teal500 },
 	["@lsp.type.lifetime.rust"] = { fg = colors.skyblue300 },
+	["@lsp.type.macro.rust"] = c.Macro,
+	["@lsp.type.decorator.rust"] = c.Macro,
 	LspReferenceText = c.IncSearch,
 	LspReferenceRead = c.IncSearch,
 	LspReferenceWrite = c.IncSearch,
