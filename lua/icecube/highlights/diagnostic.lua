fc08ec91e39dab1e3a4550cb756aeff7520e6bb5
mwcz <mwc@clayto.com>
2024-05-29 09:53:55 -0400

    improve look of unused variables

--- lua/icecube/highlights/diagnostic.lua
+++ lua/icecube/highlights/diagnostic.lua
@@ -10,6 +10,7 @@ return {
 	DiagnosticUnderlineWarn = { underline = false },
 	DiagnosticUnderlineInfo = { underline = false },
 	DiagnosticUnderlineHint = { underline = false },
+	DiagnosticUnnecessary = { fg = c.Comment.fg, underline = true },
 
 	-- Unsure if these will be needed
 	-- DiagnosticFloatingError = { fg=c.Error.fg, },
